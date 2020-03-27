28f260ab8a51bb4f9af4b8f1aaece4ee01a347d6
yasuhiro <yasucom4646@gmail.com>
2020-03-27 02:51:30 -0700

    Added InnerContainer styled component

--- client/src/styles/common.styled.component.js
+++ client/src/styles/common.styled.component.js
@@ -5,3 +5,10 @@ export const Container = styled.div`
     width: 100%;
     margin: auto;
 `;
+
+export const InnerContainer = styled.div`
+    max-width: 1088px;
+    width: 100%;
+    margin: auto;
+    padding: 10rem 0;
+`;
