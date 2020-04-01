cd36f6443028ad52d82051296c4d169e6dd677ed
yasuhiro <yasucom4646@gmail.com>
2020-04-01 15:56:36 -0700

    Added font awesome and added copyright and sns links

--- client/src/components/header/header.component.tsx
+++ client/src/components/header/header.component.tsx
@@ -1,33 +1,41 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
-import { Container } from '../../styles/common.styled.component';
-import { HeaderBar, Logo, Nav } from './header.style';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebookSquare, faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { Logo, Nav } from '../../styles/common.style.component';
+import { HeaderBar, CopyRight, HeaderSns } from './header.style';
 
 const Header = () => {
     return(
+        <>
         <HeaderBar>
-            <Container>
-                <Logo><Link to='/'><img src="/images/logo.svg" alt="Logo:Chapitredeux"/></Link></Logo>
-                <Nav>
-                    <li>
-                        <Link to='/'>Home</Link>
-                    </li>
-                    <li>
-                        <Link to='/about'>About</Link>
-                    </li>
-                    <li>
-                        <Link to='/collection'>Collection</Link>
-                    </li>
-                    <li>
-                        <Link to='/blog'>Blog</Link>
-                    </li>
-                    <li>
-                        <Link to='/contact'>Contact</Link>
-                    </li>
-                </Nav>
-            </Container>
+            <Logo><Link to='/'><img src="/images/logo.svg" alt="Logo:Chapitredeux"/></Link></Logo>
+            <Nav>
+                <li>
+                    <Link to='/'>Home</Link>
+                </li>
+                <li>
+                    <Link to='/about'>About</Link>
+                </li>
+                <li>
+                    <Link to='/collection'>Collection</Link>
+                </li>
+                <li>
+                    <Link to='/blog'>Blog</Link>
+                </li>
+                <li>
+                    <Link to='/contact'>Contact</Link>
+                </li>
+            </Nav>
         </HeaderBar>
+        <CopyRight>
+            <p>© 2019 Chapitredeux All Rights Reserved.</p>
+        </CopyRight>
+        <HeaderSns>
+            <li><a href=''><FontAwesomeIcon icon={faFacebookSquare} /></a></li>
+            <li><a href=''><FontAwesomeIcon icon={faInstagram} /></a></li>
+        </HeaderSns>
+        </>
     )
 }
 
--- client/src/components/header/header.style.js
+++ client/src/components/header/header.style.js
@@ -1,32 +1,46 @@
 import styled from 'styled-components';
-import { colors } from '../../styles/common.styles';
+import { colors } from '../../styles/common.style';
 
 export const HeaderBar = styled.header`
-    padding: 2rem 0;
-    div {
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-    }
-`;
-
-export const Logo = styled.h1`
-    img {
-        width: 20rem;
-        height: auto;
-    }
-`;
-
-export const Nav = styled.ul`
+    width: calc(100% - 4rem);
+    margin: auto;
+    padding: 2rem 2rem;
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
     display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background-color: ${colors.colorWhite};
+    z-index: 100;
+`;
+
+export const CopyRight = styled.div`
+    position: fixed;
+    left: 1.08%;
+    top: 0px;
+    bottom: 0;
+    writing-mode: vertical-rl;
+    text-align: center;
+    p {
+        font-size: 1rem;
+    }
+`;
+
+export const HeaderSns = styled.ul`
+    height: fit-content;
+    margin: auto;
+    position: fixed;
+    top: 0;
+    right: 1.2rem;
+    bottom: 0;
     li {
-        margin-left: 2rem;
         &:first-child {
-            margin-left: 0;
+            margin-bottom: 1rem;
         }
         a {
-            font-size: 2rem;
-            color: ${colors.colorGold};
+            font-size: 2.6rem;
         }
     }
 `;
