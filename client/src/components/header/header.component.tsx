d37247394823b7c03bc07e0f2f6b4b74f92bbea4
yasuhiro <yasucom4646@gmail.com>
2020-04-02 02:03:42 -0700

    Added Hamburger menu

--- client/src/components/header/header.component.tsx
+++ client/src/components/header/header.component.tsx
@@ -1,32 +1,148 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React,{ useRef, useState } from 'react';
+import { TweenLite, Expo } from 'gsap';
+import { useHistory } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFacebookSquare, faInstagram } from '@fortawesome/free-brands-svg-icons';
-import { Logo, Nav } from '../../styles/common.style.component';
-import { HeaderBar, CopyRight, HeaderSns } from './header.style';
+import { Logo } from '../../styles/common.style.component';
+import { HeaderBar,HeaderMenu, Hamburger, LoginCartNav, CopyRight, HeaderSns } from './header.style';
+import { colors } from '../../styles/common.style';
 
 const Header = () => {
+    const hamburger = useRef(null);
+    const menu = useRef(null);
+    const logo = useRef(null);
+    const [menuToggle, setMenuToggle] = useState<boolean | null>(true);
+    let navList:Array<Element> = [];
+    let bars:Array<Element> = [];
+    let history = useHistory();
+    const changePage = (e: any, destination:string) => {
+        e.preventDefault();
+        onCloseMenu();
+        setTimeout(() => {
+          history.push(destination);
+        }, 950);
+    };
+    const onClickMenu = () => {
+        setMenuToggle(!menuToggle);
+        if(menuToggle === true) {
+            TweenLite.to(menu.current, 1, {
+                width: '100%',
+                ease: Expo.easeInOut
+            });
+            TweenLite.to(bars[0], 0.5, {
+                transform: 'rotate(48deg) translateY(7px) translateX(9px)',
+                backgroundColor:'white'
+            });
+            TweenLite.to(bars[1], 0.5, {
+                width: '100%',
+                transform: 'rotate(-48deg)',
+                backgroundColor:'white'
+            });
+            TweenLite.to(bars[2], 0.5, {
+                opacity: 0
+            });
+            TweenLite.to(navList[0], 0.5, {
+                delay: 1,
+                transform: 'translateY(0)',
+            });
+            TweenLite.to(navList[1], 0.5, {
+                delay: 1.3,
+                transform: 'translateY(0)'
+            });
+            TweenLite.to(navList[2], 0.5, {
+                delay: 1.6,
+                transform: 'translateY(0)'
+            });
+            TweenLite.to(navList[3], 0.5, {
+                delay: 1.8,
+                transform: 'translateY(0)'
+            });
+            TweenLite.to(navList[4], 0.5, {
+                delay: 2,
+                transform: 'translateY(0)'
+            });
+        } else {
+            onCloseMenu();
+        }        
+    }
+    const onCloseMenu = () => {
+        setMenuToggle(true);
+        TweenLite.to(navList[0], 0.5, {
+            transform: 'translateY(100%)'
+        });
+        TweenLite.to(navList[1], 0.5, {
+            delay: 0.2,
+            transform: 'translateY(100%)'
+        });
+        TweenLite.to(navList[2], 0.5, {
+            delay: 0.4,
+            transform: 'translateY(100%)'
+        });
+        TweenLite.to(navList[3], 0.5, {
+            delay: 0.6,
+            transform: 'translateY(100%)'
+        });
+        TweenLite.to(navList[4], 0.5, {
+            delay: 0.8,
+            transform: 'translateY(100%)'
+        });
+        TweenLite.to(menu.current, 0.5, {
+            delay: 1,
+            width: 0,
+            ease: Expo.easeInOut
+        });
+        TweenLite.to(bars[0], 0.5, {
+            delay: 1,
+            transform: 'unset',
+            backgroundColor:colors.colorBlue
+        });
+        TweenLite.to(bars[1], 0.5, {
+            delay: 1,
+            width: '70%',
+            transform: 'unset',
+            backgroundColor:colors.colorBlue
+        });
+        TweenLite.to(bars[2], 0.5, {
+            delay: 1,
+            opacity: 1
+        });
+    }
     return(
         <>
         <HeaderBar>
-            <Logo><Link to='/'><img src="/images/logo.svg" alt="Logo:Chapitredeux"/></Link></Logo>
-            <Nav>
+            <Hamburger className='hamburger' onClick={() => {onClickMenu()}} ref={hamburger}>
+                <span ref={(e) => { e !== null && bars.push(e) }}></span>
+                <span ref={(e) => { e !== null && bars.push(e) }}></span>
+                <span ref={(e) => { e !== null && bars.push(e) }}></span>
+            </Hamburger>
+            <HeaderMenu ref={menu}>
+                <menu>
+                    <li>
+                        <a onClick={(e) => changePage(e, `/works`)} ref={(e) => { e !== null && navList.push(e) }}>Home</a>
+                    </li>
+                    <li>
+                        <a onClick={e => changePage(e, `/works`)} ref={(e) => { e !== null && navList.push(e) }}>About</a>
+                    </li>
+                    <li>
+                        <a onClick={e => changePage(e, `/works`)} ref={(e) => { e !== null && navList.push(e) }}>Collection</a>
+                    </li>
+                    <li>
+                        <a onClick={e => changePage(e, `/works`)} ref={(e) => { e !== null && navList.push(e) }}>Blog</a>
+                    </li>
+                    <li>
+                        <a onClick={e => changePage(e, `/works`)} ref={(e) => { e !== null && navList.push(e) }}>Contact</a>
+                    </li>
+                </menu>
+            </HeaderMenu>
+            <Logo><a onClick={e => changePage(e, `/works`)}><img src="/images/logo.svg" alt="Logo:Chapitredeux"/></a></Logo>
+            <LoginCartNav>
                 <li>
-                    <Link to='/'>Home</Link>
+                    <a onClick={e => changePage(e, `/works`)}><FontAwesomeIcon icon={faFacebookSquare} /></a>
                 </li>
                 <li>
-                    <Link to='/about'>About</Link>
+                    <a onClick={e => changePage(e, `/works`)}><FontAwesomeIcon icon={faFacebookSquare} /></a>
                 </li>
-                <li>
-                    <Link to='/collection'>Collection</Link>
-                </li>
-                <li>
-                    <Link to='/blog'>Blog</Link>
-                </li>
-                <li>
-                    <Link to='/contact'>Contact</Link>
-                </li>
-            </Nav>
+            </LoginCartNav>
         </HeaderBar>
         <CopyRight>
             <p>© 2019 Chapitredeux All Rights Reserved.</p>
--- client/src/components/header/header.style.js
+++ client/src/components/header/header.style.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import { colors } from '../../styles/common.style';
+import { Nav } from '../../styles/common.style.component';
 
 export const HeaderBar = styled.header`
     width: calc(100% - 4rem);
@@ -10,10 +11,88 @@ export const HeaderBar = styled.header`
     left: 0;
     right: 0;
     display: flex;
-    justify-content: space-between;
+    justify-content: center;
     align-items: center;
     background-color: ${colors.colorWhite};
     z-index: 100;
+`;
+
+export const HeaderMenu = styled.nav`
+    width:0;
+    height:100vh;
+    position:fixed;
+    top: 0;
+    left: 0;
+    background-color:${colors.colorBlue};
+    overflow-y:scroll;
+    padding-bottom:30px;
+    display: flex;
+    align-items: center;
+    menu {
+        margin:60px auto;
+        position:relative;
+        text-align:center;
+        li {
+            margin-top: 2rem;
+            overflow: hidden;
+            &:first-child {
+                margin-top: 2rem;
+            }
+        }
+        a {
+            padding-bottom: 1rem;
+            transform: translateY(100%);
+            display: block;
+            color: ${colors.colorWhite};
+            font-size: 5rem;
+            position: relative;
+            &:before {
+                content: '';
+                width: 0;
+                height: 2px;
+                position: absolute;
+                left: 0;
+                bottom: 0;
+                background-color: ${colors.colorWhite};
+                transition:0.6s;
+            }
+            &:hover:before {
+                width: 100%;
+            }
+        }
+    }
+`;
+
+export const Hamburger = styled.div`
+    width:4.5rem;
+    height:5.0rem;
+    position: absolute;
+    top:2rem;
+    left: 2rem;
+    z-index:10;
+    transition:0.3s;
+    cursor: pointer;
+    &:hover {
+        width:5.5rem;
+    }
+    span {
+        width:100%;
+        height:3px;
+        margin: 10px 0;
+        background-color:${colors.colorBlue};
+        display:block;
+    }
+    span:nth-child(2) {
+        width:70%;
+    }
+    span: last-child {
+        width:50%;
+    }
+`;
+
+export const LoginCartNav = styled(Nav)`
+    position: absolute;
+    right: 2rem;
 `;
 
 export const CopyRight = styled.div`
