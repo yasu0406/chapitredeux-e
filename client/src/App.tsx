ad12302dde57189cf1a0b2525a4e2f18a063e66b
yasuhiro <yasucom4646@gmail.com>
2020-04-29 16:19:41 -0700

    Added Route for showing Collection CollectionDetail

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,9 +4,11 @@ import {
   Route
 } from 'react-router-dom';
 import Header from './components/header/header.component';
+import Footer from './components/footer/footer.component';
 import Home from './pages/home/homepage.component';
 import About from './pages/about/about.component';
-import Footer from './components/footer/footer.component';
+import Collection from './pages/collection/collection.component';
+import CollectionDetail from './pages/collection-detail/colection-detail.component';
 
 function App() {
   return (
@@ -15,6 +17,8 @@ function App() {
       <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/about" component={About} />
+          <Route exact path="/collection" component={Collection} />
+          <Route exact path="/collection-detail/:title,:id" component={CollectionDetail} />
       </Switch>
       <Footer />
     </div>
